5f540e95588ce446f2135e9e2392ece995c57c91
Ahmet Erdem Kahveci <ahmeterdemkahveci@gmail.com>
2018-05-20 01:31:05 +0300

    refs:#2 springboot configuration

---
+++ src/main/java/springbootstarter/CourseApiApp.java
@@ -0,0 +1,13 @@
+package springbootstarter;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
